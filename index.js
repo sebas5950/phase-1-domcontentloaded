117a4b1d19dcb68ce90244596ad423ce334153ce
Alex Guanipatin <sebas5950@gmail.com>
2022-05-23 13:45:29 -0400

    commit tree 5ae278037805ea963fc8aae4bb69342df02f029d

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const p = document.querySelector('#text')
+    p.textContent = 'This is really cool!'
+    console.log(p)
+})
+console.log('theDom has not loaded')
